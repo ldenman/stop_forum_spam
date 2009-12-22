c73dd6281937b8be3af0f50977e6d61ee058fe83
Lake Denman <lake@lakedenman.com>
2009-12-21 22:10:02 -0500

    back to green

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,8 @@ require File.join(File.dirname(__FILE__), '..', 'lib', 'stop_forum_spam')
 
 def fake_get_response(params, options={})
   url = "http://stopforumspam.com/api?" + params
-  type, value = params.split('=')
-  options[:body] = format_options(options)
+  options[:type], value = params.split('=')
+  options[:string] = format_options(options)
   FakeWeb.register_uri(:get, url, options)
 end
 
@@ -16,7 +16,5 @@ def format_options(options)
   appears = options[:appears]
   frequency = options[:frequency]
   last_seen = options[:last_seen]
-  
-  response = "<response success='true'><type>#{type}</type><appears>#{appears ? 'yes' : 'no'}</appears><frequency>#{appears ? '0' : frequency}</frequency><lastseen>#{last_seen}</lastseen></response>"
-  
+  "<response success='true'><type>#{options[:type]}</type><appears>#{appears ? 'yes' : 'no'}</appears><frequency>#{appears ? '0' : frequency}</frequency><lastseen>#{last_seen}</lastseen></response>"
 end
